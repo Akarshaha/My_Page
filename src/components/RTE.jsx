09b3a2a89da546e54860c5f5e6b198bd58f04b57
Akarshaha <61219561+Akarshaha@users.noreply.github.com>
2024-09-25 00:37:33 +0530

    Update RTE.jsx

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -4,6 +4,8 @@ import {Controller } from 'react-hook-form';
 
 
 export default function RTE({name, control, label, defaultValue =""}) {
+  var api = import.meta.env.VITE_TINYMCE_API_KEY
+
   return (
     <div className='w-full'> 
     {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
@@ -18,7 +20,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
             initialValue: defaultValue,
             height: 500,
             menubar: true,
-            apiKey: import.meta.env.VITE_TINYMCE_API_KEY,
+            apiKey: api,
             plugins: [
                 "image",
                 "advlist",
